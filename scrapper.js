253c0ad9046fb1181e5774ee439c7f8b60ac3b95
Aryan Kanwar <kanwararyan1@gmail.com>
2017-08-06 22:51:09 +0530

    Corrected Indentation errors

--- scrapper.js
+++ scrapper.js
@@ -2,7 +2,7 @@ const express  = require('express');
 const cheerio  = require('cheerio');
 const notifier = require('node-notifier');
 const request  = require('request');
-const CronJob    = require('cron').CronJob;
+const CronJob  = require('cron').CronJob;
 
 var job = new CronJob({
   cronTime: '0 */45 * * * *',
@@ -10,18 +10,18 @@ var job = new CronJob({
   	request('http://timesofindia.indiatimes.com', function (error, response, html) {
 		if (!error && response.statusCode == 200) {
 			console.log('Fetching latest news from Times of India');
- 			var $ 		   = cheerio.load(html); //loading hrml content through cheerio
-    		var ulList     = $('.list9').text(); //this ullist string contains list of latest news
+ 			var $          = cheerio.load(html); //loading hrml content through cheerio
+    	  	        var ulList     = $('.list9').text(); //this ullist string contains list of latest news
    			var listArray  = ulList.split("\n"); //converting ulList string to array
-    		var cleanArray = listArray.filter(function(e){ return e.replace(/(\r\n|\n|\r)/gm,"")}); //remove empty elements from news list array 
+    	         	var cleanArray = listArray.filter(function(e){ return e.replace(/(\r\n|\n|\r)/gm,"")}); //remove empty elements from news list array 
 			//To dispaly notification in ubuntu
 			notifier.notify({
-    			title   : 'Latest News',
-     			message : cleanArray[0],  //Fetching lastest news
+				title   : 'Latest News',
+     				message : cleanArray[0],  //Fetching lastest news
 				sound   : true, // Only Notification Center or Windows Toasters 
   				wait    : false // Wait with callback, until user action is taken against notification 
   			});
-	    }
+		}
 	}); 
   },
   start: false
